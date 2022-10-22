ef4f2e84d6f5c49788745832392f2842c9f6f68d
KenMR <kmr5591@gmail.com>
2022-10-21 20:51:25 -0400

    Add code to make the squares change color on mouse over

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,18 @@
 const box = document.getElementById("box");
+let cell;
 
 function makeGrid(rows, cols) {
   box.style.setProperty('--grid-rows', rows);
   box.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
-    let cell = document.createElement("div");
+    cell = document.createElement("div");
     box.appendChild(cell).className = "grid-element";
   };
 };
 
 makeGrid(16, 16);
+
+let active = document.querySelectorAll(".grid-element");
+const changeBackgroundColor = e => {e.target.classList.add('clicked');}
+
+active.forEach(div => {div.addEventListener("mouseover", changeBackgroundColor);}) //change from click
